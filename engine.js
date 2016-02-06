3dbad8a4cf9383ddc8a2f2a1224cd8986bd74564
Spiros94 <cont@eyrhka.gr>
2016-02-06 13:46:00 +0200

    Created another JS file for trivial function needed only for support

--- engine.js
+++ engine.js
@@ -140,27 +140,6 @@ init = function()
 }
 
 
-function getCursorCoords(e)
-{
-	var pos = getMousePos(canvas, e);
-    var x = pos.x;
-    var y = pos.y;
-	if(x < 500 && y < 300)
-												// Center the Axis in the base top
-		cursor_pos = "Cursor Coordinates: X=" + (x - baseTop.x) + ", Y=" + (300-y-base.height);
-}
-
-window.addEventListener('mousemove',getCursorCoords , false);
-
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-}
-
-
 FK2DofPos = function(theta1, theta2)
 {
 	// Calculated Posisition
@@ -223,20 +202,6 @@ function RunIK()
 	IKtarget.theta2Previous = parseFloat(document.getElementById("angle2Range").value);
 	EnDisDiv("myinput", true);	
 	IKrun = true;
-}
-
-
-function clog(msg)
-{
-	var innerMsg = document.getElementById("logp").innerHTML;
-	// Check if are the same to prevent rewriting the same
-	if(innerMsg != msg) document.getElementById("logp").innerHTML = "> "+ msg;
-}
-
-function ptco(msg)
-{
-	console.log("%c%s", "padding: 0 5px 0 5px;color: rgb(68, 230, 68); background: black; font-size: 18px; font-weigth: bold;", msg);
-	return true;
 }
 
 function checkForNan() // Variadic function for checking NaN
@@ -325,6 +290,8 @@ function CalcInnerRange(l1, l2, maxTheta2)
 	
 	return Math.sqrt(Math.pow(h1,2)+Math.pow(w1, 2));
 }
+
+
 
 
 
